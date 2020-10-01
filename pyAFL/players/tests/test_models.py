f36048660e72d545e6c2777276e2957f8a89a72a
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2020-10-01 22:01:04 +1000

    Pytest fix // global fixtures to conftest.py in root dir

--- pyAFL/players/tests/test_models.py
+++ pyAFL/players/tests/test_models.py
@@ -6,18 +6,7 @@ from pyAFL.players.models import Player
 from pyAFL.requests import requests
 
 
-@pytest.fixture(scope="session")
-def test_html_cache_path(tmpdir_factory):
-    fn = tmpdir_factory.mktemp("__HTMLCACHE__")
-    return fn
-
-
 class TestPlayerModel:
-    @pytest.fixture
-    def mock_html_cache_dir(self, monkeypatch, test_html_cache_path):
-        monkeypatch.setattr(requests, "get_html_cache_path", mock.Mock())
-        requests.get_html_cache_path.return_value = str(test_html_cache_path)
-
     def test_player_name_none_throws_exception(self):
         with pytest.raises(TypeError):
             Player()
@@ -45,3 +34,5 @@ class TestPlayerModel:
             Player("Babe Ruth")._get_player_url()
 
         assert "Found no players with name" in str(e)
+
+    # def test_player_classmethod_get_player_stats(self, mock_html_cache_dir):
--- pyAFL/requests/tests/test_requests.py
+++ pyAFL/requests/tests/test_requests.py
@@ -6,18 +6,7 @@ from unittest import mock
 from pyAFL.requests import requests
 
 
-@pytest.fixture(scope="session")
-def test_html_cache_path(tmpdir_factory):
-    fn = tmpdir_factory.mktemp("__HTMLCACHE__")
-    return fn
-
-
 class TestRequestCaching:
-    @pytest.fixture
-    def mock_html_cache_dir(self, monkeypatch, test_html_cache_path):
-        monkeypatch.setattr(requests, "get_html_cache_path", mock.Mock())
-        requests.get_html_cache_path.return_value = str(test_html_cache_path)
-
     @pytest.fixture
     def mock_python_get_request(self, monkeypatch):
         resp = python_requests.models.Response()
